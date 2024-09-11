af59cf07e9bb364b0a1af7ec62b014c88e74e09a
alikharrati <108900028+alikharrati@users.noreply.github.com>
2024-09-11 13:43:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function convertPdfToText() {
+    const apiKey = '92cb624e0c7213e092cf98e96d282390'; // جایگزین با کلید API شما
+    const username = 'alikharrati'; // جایگزین با نام کاربری شما
+
+    const pdfFile = document.getElementById('pdfFile').files[0];
+    if (!pdfFile) {
+        alert("Please upload a PDF file");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', pdfFile);
+
+    fetch('https://api.pdfcrowd.com/convert/pdf-to-text/', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + btoa(username + ':' + apiKey),
+        },
+        body: formData
+    })
+    .then(response => response.text())  // تبدیل پاسخ به متن
+    .then(text => {
+        document.getElementById('result').innerText = text;  // نمایش متن در صفحه
+    })
+    .catch(error => {
+        console.error("Error converting PDF to text:", error);
+    });
+}
